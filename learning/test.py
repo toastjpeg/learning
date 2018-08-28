9b47911d2e8bc57523559005a391ff6355ed12de
Langley Guin <langleyguin@Langleys-MacBook-Pro.local>
2018-08-27 22:02:42 -0500

    first program ever!!1

---
+++ learning/test.py
@@ -0,0 +1,20 @@
+my_name = "Langley"
+my_age = 23
+
+stranger_name="Jasper"
+if stranger_name=="Omu":
+    stranger_is_nice=True
+else:
+    stranger_is_nice=False
+
+nice_greeting="Hi! It's nice to meet you!"
+mean_greeting="Hey, fuck you buddy!"
+
+
+print("Hi! My name is "+my_name)
+print("Meow, "+my_name+", nyaa "+stranger_name+"-chan~")
+
+if stranger_is_nice:
+    print(nice_greeting)
+else:
+    print(mean_greeting)
